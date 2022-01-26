e5c8ab0a455c8e5120e63604cc8ef5d2fd653536
Josh Smith <josh.smith@demos.co.uk>
2022-01-26 11:30:18 +0000

    Moved search queries to query_terms.py, adjusted .gitignore, added data folder

---
+++ query_terms.py
@@ -0,0 +1,4 @@
+test_titles = ['Butoconazole', 'Shaykh_Tabarsi']
+
+# Fill 'titles' with the page titles you'd like to query
+titles = []
--- wiki_histories.py
+++ wiki_histories.py
@@ -2,25 +2,9 @@ import wikipedia_histories
 import pandas
 import os
 
-test_titles = ['Butoconazole', 'Shaykh_Tabarsi']
-titles = [
-"Xinjiang",
-"Chinese_Communist_Party",
-"East_Turkestan_independence_movement",
-"Tibetan_independence_movement",
-"Human_rights_in_China",
-"Chinese_intelligence_activity_abroad",
-"Maoism",
-"Falun_Gong",
-"Chinese_language",
-"Chinese_television",
-"Taiwan",
-"Tibet",
-"Xinjiang_re-education_camps",
-"Tiananmen_Square_protests_of_1989",
-"WeChat",
-"Mao_Zedong",
-"Xi_Jinping"]
+# Titles to search are defined in query_terms.py, so this can be kept out of 
+# commits.
+from query_terms import titles
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(root_dir, 'data')
